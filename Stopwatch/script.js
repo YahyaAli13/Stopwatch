33b3d1cdc032718bb12bbd23f3aff022ba98ccfb
YahyaAli13 <160343780+YahyaAli13@users.noreply.github.com>
2024-02-29 18:06:20 -0500

    Add files via upload
    A Stopwatch which was built using HTML, CSS, and JavaScript

---
+++ Stopwatch/script.js
@@ -0,0 +1,52 @@
+let[millisecond, second, minutes, hours] = [0, 0, 0,0];
+let timeref = document.querySelector(".timer-display");
+let int = null;
+
+document.getElementById("start-timer").addEventListener("click",
+() => {
+    if(int !== null){
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer, 10);
+
+});
+
+document.getElementById("pause-timer").addEventListener("click",
+() => {
+    clearInterval(int);
+});
+
+document.getElementById("reset-timer").addEventListener("click",
+() => {
+    clearInterval(int);
+    [millisecond, second, minutes, hours] = [0, 0, 0,0];
+    timeref.innerHTML = "00 : 00 : 00 : 000 ";
+
+});
+
+function displayTimer(){
+    millisecond += 10;
+    if(millisecond == 1000){
+        millisecond = 0
+        second++;
+        if(second == 60){
+            second = 0;
+            minutes++;
+            if(minutes == 60){
+                minutes = 0;
+                hours++
+            }
+        }
+    }
+
+    let h = hours < 10 ? "0" + hours: hours;
+    let m = minutes < 10 ? "0" + minutes: minutes;
+    let s = second < 10 ? "0" + second: second;
+    let ms = 
+        millisecond < 10 
+        ? "00" + millisecond
+        : millisecond < 100
+        ? "0" + millisecond
+        :millisecond;
+    timeref.innerHTML = `${h}: ${m} : ${s}: ${ms}`;    
+}
